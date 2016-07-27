6cff5a626bf2d2e6e602371f516e0a77d330cca4
yoer <iamyoer@gmail.com>
2016-07-27 18:02:52 +0800

    global header

---
+++ source/include/qrwidgets_global.h
@@ -0,0 +1,22 @@
+﻿#ifndef QRWIDGETS_GLOBAL_H
+#define QRWIDGETS_GLOBAL_H
+
+#include <QtCore/qglobal.h>
+
+#if defined(QRWIDGETS_LIBRARY)
+#  define QRWIDGETSSHARED_EXPORT Q_DECL_EXPORT
+#else
+#  define QRWIDGETSSHARED_EXPORT Q_DECL_IMPORT
+#endif
+
+//  qrwidgets namespace
+#define NS_QRWIDGETS_BEGIN \
+    namespace Qters { \
+    namespace QrWidgets {
+#define NS_QRWIDGETS_END \
+    } \
+    }
+#define USING_NS_QRWIDGETS    using namespace Qters::QrWidgets
+#define NS_QRWIDGETS  ::Qters::QrWidgets
+
+#endif // QRWIDGETS_GLOBAL_H
